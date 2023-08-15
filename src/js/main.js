3e5bda1c4ca40817c8f7c167dfe1ba34ba14c339
Claudia Villarroel <claudia.villarroel@live.com>
2023-08-15 00:21:24 -0400

    Mejoras en despliegue de input

--- src/js/main.js
+++ src/js/main.js
@@ -66,10 +66,7 @@ const DesplegarDetalleTareas = (JSON = tareasJSON) => {
             <div class="row mt-2">
                 <div class="col-md-2 text-right">${tarea.id}</div>
                 <div class="col-md-8">${tarea.descripcion}</div>
-                <div class="col-md-1"><input class="form-check-input" type="checkbox" id="check${tarea.id}" onclick="ModificarEstado(${tarea.id})" `; 
-        if(tarea.completado) 
-            template +=  ` checked `;
-        template += `></input></div>
+                <div class="col-md-1"><input class="form-check-input" type="checkbox" id="check${tarea.id}" ${tarea.completado ? 'checked' : ''} onclick="ModificarEstado(${tarea.id})"></input></div>
                 <div class="col-md-1"><button class="btn-danger" onclick="EliminarTarea(${tarea.id})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
               </svg></button></div>

5d80dff6a8605d694affc044a3d30a40551feb50
Claudia Villarroel <claudia.villarroel@live.com>
2023-08-15 00:13:53 -0400

    ADD: Ajustes finales

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,18 @@
-const tareasJSON = [{
-    id:1,
-    descripcion:"estudiar ingles",
-    completado:true
-},{
-    id:2,
-    descripcion:"cantar",
-    completado:false
-},
-{
-    id:3,
-    descripcion:"lavar losa",
-    completado:false
-},
-{
-    id:3,
-    descripcion:"lavar losa",
-    completado:true
-}
+const tareasJSON = [
+    {
+        id:1,
+        descripcion:"Estudiar ingles",
+        completado:false
+    },{
+        id:2,
+        descripcion:"Preparar Desafio",
+        completado:false
+    },
+    {
+        id:3,
+        descripcion:"Entregar desafio",
+        completado:true
+    },
 ];
 
 const btnAgregar = document.getElementById("btnAgregar");
@@ -36,50 +32,52 @@ const ObtenerMaximoId = () => {
 
 btnAgregar.addEventListener("click",function(){
     inputTarea = document.getElementById("inputTarea").value;
-    const nuevaTarea = {
+    if(inputTarea.trim()!=""){
+        const nuevaTarea = {
         id : ObtenerMaximoId()+1,
         descripcion : inputTarea,
         completado : false
-    };
-    tareasJSON.push(nuevaTarea);
-    document.getElementById("inputTarea").value = "";
-    DesplegarDetalleTareas();
-    DesplegarTotal();
-    DesplegarCompletados();
+       };
+        tareasJSON.push(nuevaTarea);
+        document.getElementById("inputTarea").value = "";
+        DesplegarDetalleTareas();
+    }
+    else{
+        $('#modal').modal('show');
+    }
 });
 
 const EliminarTarea = (idTarea) => {
     const index = tareasJSON.findIndex((tarea)=> tarea.id == idTarea);
     tareasJSON.splice(index,1);
     DesplegarDetalleTareas();
-    DesplegarTotal();
-    DesplegarCompletados();
+}
+
+const ModificarEstado = (idTarea) => {
+    const index = tareasJSON.findIndex((tarea)=> tarea.id == idTarea);
+    tareasJSON[index].completado = !tareasJSON[index].completado;
+    DesplegarDetalleTareas();
 }
 
 const DesplegarDetalleTareas = (JSON = tareasJSON) => {
     let template = "";
     JSON.forEach((tarea) => {
         template += `
-            <div class="row m-1">
-                <div class="col-1">${tarea.id}</div>
-                <div class="col-3">${tarea.descripcion}</div>
-                <div class="col-2">${tarea.completado}</div>
-                <div class="col-2"><button class="btn-danger" onclick="EliminarTarea(${tarea.id})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+            <div class="row mt-2">
+                <div class="col-md-2 text-right">${tarea.id}</div>
+                <div class="col-md-8">${tarea.descripcion}</div>
+                <div class="col-md-1"><input class="form-check-input" type="checkbox" id="check${tarea.id}" onclick="ModificarEstado(${tarea.id})" `; 
+        if(tarea.completado) 
+            template +=  ` checked `;
+        template += `></input></div>
+                <div class="col-md-1"><button class="btn-danger" onclick="EliminarTarea(${tarea.id})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
               </svg></button></div>
-            </div>
-        `;
+            </div>`;
     });
     detalle_tareas.innerHTML = template;
-}
-
-const DesplegarTotal = () => {
     cant_total.innerHTML = tareasJSON.length;
-}
-
-const DesplegarCompletados = () => {
     cant_realizadas.innerHTML = tareasJSON.filter(tarea => tarea.completado==true).length;
 }
+
 DesplegarDetalleTareas();
-DesplegarTotal();
-DesplegarCompletados()
